f5b0df870e3d8c678cab6c3ff9fec89f3a9bf7f9
ranabirjashu3-code <ranabirjashu3@gmail.com>
2026-01-20 17:29:45 +0000

    master

---
+++ date_and_time.js
@@ -0,0 +1,10 @@
+let mydate=new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+
+let createdDate=new Date(2020,0,23)
+console.log(createdDate.toLocaleString());
+let createdDate1=new Date("01-20-2026")
+console.log(createdDate1.toLocaleString());
+
