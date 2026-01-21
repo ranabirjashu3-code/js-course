86ae87bfe9220050a3e79bde4c939574b3aec22c
ranabirjashu3-code <ranabirjashu3@gmail.com>
2026-01-21 17:50:56 +0000

    master

---
+++ array2.js
@@ -0,0 +1,24 @@
+const marvel_hero=["Spider man","Ironman","Hulk"]
+const Dc_hero=["Superman","Batman","flash"]
+
+// marvel_hero.push(Dc_hero)
+// console.log(marvel_hero);
+// console.log(marvel_hero[3][2]);
+
+// const allhero=marvel_hero.concat(Dc_hero)
+// console.log(allhero);
+
+// const allnewhero=[...marvel_hero,...Dc_hero]
+// console.log(allnewhero);
+
+// const another_array=[1,2,3,[4,5,6,[7,8,9]]]
+// const real_arr=another_array.flat(Infinity)
+// console.log(real_arr);
+
+console.log(Array.isArray("Ranbir"));
+console.log(Array.from("Ranbir"));
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3));
