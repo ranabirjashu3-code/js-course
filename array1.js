8357a1f761f72fe8c0d5fd4289e46fd0d79e8d3f
ranabirjashu3-code <ranabirjashu3@gmail.com>
2026-01-21 17:28:01 +0000

    master

---
+++ array1.js
@@ -0,0 +1,31 @@
+const myarr=[0,1,2,3,4,5]    //array declaration
+const myhero=["Spider man","Ironman","Hulk"]
+
+const mynum=new Array(1,2,3,4,5)  //another way to declare array
+//console.log(mynum[0]);
+
+
+//***********Array method****************/
+// myarr.push(6)
+// console.log(myarr);
+// myarr.pop()
+// console.log(myarr);
+// myarr.unshift(7)
+// myarr.shift()
+// console.log(myarr);
+// console.log(myarr.includes(3));
+// console.log(myarr.indexOf(5));
+
+// const newarr=myarr.join()   //adds all the element into string
+// console.log(newarr);
+console.log("A:",myarr);
+const arr1=myarr.slice(1,3)
+console.log(arr1);
+console.log("B:",myarr);
+
+const arr2=myarr.splice(1,3)
+console.log(arr2);
+console.log("B:",myarr);
+
+
+
