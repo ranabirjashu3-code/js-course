13159f24b96b4f672ae27dbfddc0d9f28a7219d2
ranabirjashu3-code <ranabirjashu3@gmail.com>
2026-01-27 17:19:35 +0000

    master

---
+++ while_loop.js
@@ -0,0 +1,40 @@
+//************while loop******************* */
+
+// let i=0
+// while (i<=10) {
+//     console.log(i);
+//     i=i+1
+// }
+
+//-------------------------------------------
+
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+// let i=0
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++  
+// }
+
+//-------------------do while------------------------
+
+
+
+// let score=1
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+    
+// } while (score <= 10);
+
+//---------------------------------------------------
+
+//first do then check
+
+let score=12
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <= 10);
